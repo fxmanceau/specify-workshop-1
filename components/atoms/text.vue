4e96d1ad42cd22c4d32627a9eff19c0ecc7e71fd
fxmanceau <francois-xavier.manceau@hetic.net>
2020-11-17 09:46:59 +0100

    :sparkles: add basic config

---
+++ components/atoms/text.vue
@@ -0,0 +1,22 @@
+<template>
+  <component :is="tag" v-html="content" :class="['a-text', {}]"></component>
+</template>
+
+<script>
+export default {
+  props: {
+    tag: {
+      type: String,
+      default: 'p',
+    },
+    content: {
+      type: String,
+      default: '',
+    },
+    style: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
---
+++ components/organisms/menu.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="o-menu col-start-1 col-end-5"></div>
+</template>
